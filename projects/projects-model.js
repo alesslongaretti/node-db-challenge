091df3852e76ac17a31e18d01bfab55db9d7ca0f
alesslongaretti <alesslongaretti@gmail.com>
2020-04-17 23:28:41 -0400

    endpoints for adding and retrieving both tasks and resources are working - tested in postman

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -8,7 +8,31 @@ function add(projectData) {
     return db('projects').insert(projectData)
 }
 
+function findResources() {
+    return db('resources')
+}
+
+function addResource(resourceData) {
+    return db('resources').insert(resourceData)
+}
+
+function findTasks() {
+    return db('tasks')
+    .join("projects", "projects.id", "tasks.project_id")
+    .select('tasks.project_id',"tasks.id","tasks.description", "tasks.notes", 
+    "tasks.completed", "projects.project-name", "projects.description")
+}
+
+
+function addTask(taskData) {
+    return db('tasks').insert(taskData)
+}
+
 module.exports = {
     find,
-    add
+    add,
+    findResources,
+    addResource,
+    findTasks,
+    addTask
 };
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,6 +4,8 @@ const Projects = require('./projects-model.js');
 
 const router = express.Router();
 
+
+// Working
 router.get('/', (req, res) => {
     Projects.find()
         .then(projects => {
@@ -14,6 +16,8 @@ router.get('/', (req, res) => {
         });
 });
 
+
+// Working
 router.post('/', (req,res) => {
     const projectData = req.body;
 
@@ -25,7 +29,59 @@ router.post('/', (req,res) => {
             res.status(500).json({ message: 'Failed to post project', err})
 
         })
-})
+});
+
+
+// Working 
+router.get('/resources', (req,res) => {
+    Projects.findResources()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(err => {
+            res.status(500).json({ message:'Failed to get resources', err})
+        })
+});
+
+
+// Working
+router.post('/resources', (req,res) => {
+    const resourceData = req.body;
+
+    Projects.addResource(resourceData)
+        .then(resource => {
+            res.status(201).json(resource);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to post resource', err})
+        })
+});
+
+
+// Working
+router.get('/tasks', (req,res) => {
+    Projects.findTasks()
+        .then(tasks => {
+            res.status(200).json(tasks)
+        })
+        .catch(err => {
+            res.status(500).json({ message:'Failed to get tasks', err})
+        })
+});
+
+
+// Working
+router.post('/tasks', (req,res) => {
+    const taskData = req.body;
+
+    Projects.addTask(taskData)
+        .then(task => {
+            res.status(201).json(task);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to post task', err})
+        })
+});
 
 
 module.exports = router;
